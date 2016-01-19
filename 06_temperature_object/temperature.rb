72c9dbc2c90f83d9bebe7c5239b311e167cc54d9
Cody Condon <codycondon50@gmail.com>
2016-01-18 20:34:55 -0500

    tests pass

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -1,6 +1,31 @@
 class Temperature
-def initialize(options={})
-  
-end
+  def initialize(temperature)
+    @fahrenheit = temperature[:f]
+    @celsius = temperature[:c]
+  end
+  #Is temperature celsius , if so all good ,if not then convert to celsis
+  def to_celsius
+    @celsius? @celsius : (@fahrenheit - 32) * 5 / 9
+  end
+#is temperature fahrenheit ? if so all good, if not convert to fahrenheit
+  def to_fahrenheit
+    @fahrenheit? @fahrenheit : (@celsius * 9 / 5) + 32
+  end
+  def self.in_celsius(temperature)
+    Temperature.new(:c => temperature)
+  end
 
+  def self.in_fahrenheit(temperature)
+    Temperature.new(:f => temperature)
+  end
+end
+class Fahrenheit < Temperature
+  def initialize(temperature)
+    @fahrenheit = temperature
+  end
+end
+class Celsius < Temperature
+  def initialize(temperature)
+    @celsius = temperature
+  end
 end
